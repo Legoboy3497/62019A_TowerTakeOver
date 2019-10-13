5aa6f01944539f8bbf6dc75bc5c0483e31ed33c4
TonyCha3713 <tonyjcha3713@gmail.com>
2019-10-13 15:10:52 -0700

    Created code

---
+++ First_Comp/include/pragma.h
@@ -0,0 +1,8 @@
+#ifndef PRAGMA_H
+#define PRAGMA_H
+extern Controller Master;
+
+
+
+
+#endif
---
+++ First_Comp/src/subsystems/base.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void runLeftBase(double input)
+{
+    leftBase1.move(input);
+    LeftBase2.move(input);
+}
+
+void runRightBase(double input)
+{
+    RightBase1.move(input);
+    RightBase2.move(input);
+}
